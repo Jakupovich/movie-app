025b296ceada3d39b76f494680effd23fc6064df
jakupovich <nordinveca@gmail.com>
2023-04-22 11:29:59 +0200

    few updates

--- src/App.js
+++ src/App.js
@@ -1,66 +1,67 @@
-import './App.css';
 import React, { useEffect, useState } from 'react';
+import './App.css';
 import Movies from './movie';
 import Navbar from "./navbar/navbar"
 import mafia from "./images/mafia.jpg"
 import patrol from "./images/patrol.jpg"
 import vin from "./images/vin.jpg"
+
 function App() {
+  const [randomState, setRandomState] = useState([]);
 
-  const [randomState,setRandomState]=useState([])
-  
-  async function getMovies(){
-    const response = await fetch("https://api.themoviedb.org/3/movie/popular?api_key=f28aa71682075a83b20e0e121c7582e3&language=en-US&page=1")
-    const data = await response.json()
-    setRandomState(data.results)
+  async function getMovies() {
+    try {
+      const response = await fetch("https://api.themoviedb.org/3/movie/popular?api_key=f28aa71682075a83b20e0e121c7582e3&language=en-US&page=1");
+      const data = await response.json();
+      setRandomState(data.results);
+    } catch (error) {
+      console.error("Error fetching movies:", error);
+    }
   }
+
   useEffect(() => {
-    getMovies()
-  },[]);
-  return(
-    
+    getMovies();
+  }, []);
+
+  return (
     <div>
-      <Navbar/>
+      <Navbar />
       <div className='content'>
-                <div className='text'>
-                    <h1 className='welcome-text'>Welcome to Norda Movies</h1>
-                    <hr></hr>
-                    <p className='big-text'>Welcome to Norda Movies. On our navigation you can choose the movies you are interested in. We offer all the latest movies, series, thrillers... We wish you a good experience on our site.</p>
-                </div>
-                <img className="Slika"src="https://travnik-2035.netlify.app/images/bg-slider-03.webp"></img>
-            </div>
-            <div className='aboutMovies'>
-            <h1 className='video-libary'>Our video library</h1>
-                <div className='threeMovies'>
-                    <div className='divs'>
-                        <img className='divs-image' src={mafia}/>
-                        <h1 className='divs-title'>Best mafia movies</h1>
-                        <p className='divs-big-text'>If you are looking for good action movies, we offer you a collection of the latest action movies</p>
-                    </div>
-                    <div className='divs'>
-                    <img className='divs-image' src={patrol}></img>
-                    <h1 className='divs-title'>Best cartoons</h1>
-                    <p className='divs-big-text'>If you are looking for cartoons for your little ones, we can offer you cartoons of world names</p>
-                    </div>
-                    <div className='divs'>
-                    <img className='divs-image' src={vin}></img>
-                    <h1 className='divs-title'>Favotite actors</h1>
-                    <p className='divs-big-text'>if you are looking for one of your favorite actors, we offer the biggest stars of acting</p>
-                    </div>
-                </div>
-                <h1 className='now-playing'>Now playing in movies</h1>
-            </div>
-    <div className='main'>
-      {randomState.map((movie)=>{
-        return(
-          <>
-                    <Movies movie={movie}/>
-</>
-
-        )
-      })}
-
-    </div></div>
-  )
+        <div className='text'>
+          <h1 className='welcome-text'>Welcome to Norda Movies</h1>
+          <hr></hr>
+          <p className='big-text'>Welcome to Norda Movies. On our navigation you can choose the movies you are interested in. We offer all the latest movies, series, thrillers... We wish you a good experience on our site.</p>
+        </div>
+        <img className="Slika" src="https://travnik-2035.netlify.app/images/bg-slider-03.webp" alt="background"></img>
+      </div>
+      <div className='aboutMovies'>
+        <h1 className='video-library'>Our video library</h1>
+        <div className='threeMovies'>
+          <div className='divs'>
+            <img className='divs-image' src={mafia} alt="mafia"></img>
+            <h1 className='divs-title'>Best mafia movies</h1>
+            <p className='divs-big-text'>If you are looking for good action movies, we offer you a collection of the latest action movies</p>
+          </div>
+          <div className='divs'>
+            <img className='divs-image' src={patrol} alt="patrol"></img>
+            <h1 className='divs-title'>Best cartoons</h1>
+            <p className='divs-big-text'>If you are looking for cartoons for your little ones, we can offer you cartoons of world names</p>
+          </div>
+          <div className='divs'>
+            <img className='divs-image' src={vin} alt="vin"></img>
+            <h1 className='divs-title'>Favorite actors</h1>
+            <p className='divs-big-text'>If you are looking for one of your favorite actors, we offer the biggest stars of acting</p>
+          </div>
+        </div>
+        <h1 className='now-playing'>Now playing in movies</h1>
+      </div>
+      <div className='main'>
+        {randomState.map((movie) => (
+          <Movies key={movie.id} movie={movie} />
+        ))}
+      </div>
+    </div>
+  );
 }
+
 export default App;
--- src/upcomming.js
+++ src/upcomming.js
@@ -1,84 +1,102 @@
 import React, { useEffect, useState } from 'react';
 import Modal from 'react-modal';
-import "./modal.css"
+import './modal.css';
 import './App.css';
 import './upcoming.css';
 import './navbar/navbar.css';
 import Navbar from './navbar/navbar';
 
-function Upcomming(props) {
-    const { movie } = props
-    const [upcomming, setUpcomming] = useState([])
-    const [modalIsOpen, setIsOpen] = useState(false);
-    async function getUpcomming() {
-        const response = await fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=f28aa71682075a83b20e0e121c7582e3&language=en-US&page=1`);
-        const data = await response.json();
-        setUpcomming(data.results)
+function Upcoming(props) {
+  const { movie } = props;
+  const [upcoming, setUpcoming] = useState([]);
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  async function getUpcoming() {
+    try {
+      const response = await fetch(
+        'https://api.themoviedb.org/3/movie/upcoming?api_key=f28aa71682075a83b20e0e121c7582e3&language=en-US&page=1'
+      );
+      const data = await response.json();
+      setUpcoming(data.results);
+    } catch (error) {
+      console.error('Error fetching upcoming movies:', error);
     }
-    useEffect(() => {
-        getUpcomming()
-    }, [])
+  }
 
-    function openModal(film) {
-        setIsOpen(film);
-    }
-    function closeModal() {
-        setIsOpen(false);
-    }
+  useEffect(() => {
+    getUpcoming();
+  }, []);
 
-    return (
+  function openModal(film) {
+    setIsOpen(film);
+  }
 
+  function closeModal() {
+    setIsOpen(false);
+  }
 
-        <>
-        <Navbar/>
-            <div className='content-upcoming'>
-                <div className='text'>
-                    <h1 className='welcome-text'>Upcoming Movies</h1>
-                    <hr></hr>
-                    <p className='big-text'>Here you can see which films will be coming to your theaters as soon as possible</p>
+  return (
+    <>
+      <Navbar />
+      <div className='content-upcoming'>
+        <div className='text'>
+          <h1 className='welcome-text'>Upcoming Movies</h1>
+          <hr></hr>
+          <p className='big-text'>
+            Here you can see which films will be coming to your theaters as soon as possible
+          </p>
+        </div>
+        <img className='Slika' src='https://travnik-2035.netlify.app/images/bg-slider-03.webp'></img>
+      </div>
+      <div className='main'>
+        {upcoming.map((film) => {
+          return (
+            <div key={film.id}>
+              <Modal isOpen={modalIsOpen === film} onRequestClose={closeModal}>
+                <div className='modalWrapper'>
+                  <img
+                    className='movie-image-modal'
+                    src={'https://image.tmdb.org/t/p/w500/' + (film?.poster_path || '')}
+                  ></img>
+                  <div onClick={closeModal} class='close-container'>
+                    <div class='leftright'></div>
+                    <div class='rightleft'></div>
+                    <label class='close'>close</label>
+                  </div>
+                  <div className='movie-text-modal'>
+                    <h1 className='movie-title-modal'>{film?.title}</h1>
+                    <p>
+                      <b>Date of release: </b>
+                      {film?.release_date}
+                    </p>
+                    <p className='movie-vote-modal'>
+                      <b>Average vote: </b>
+                      {film?.vote_average}
+                    </p>
+                    <p>
+                      <b>Film overview: </b>
+                      {film?.overview}
+                    </p>
+                  </div>
+                  <div className='modal-video'></div>
                 </div>
-                <img className="Slika"src="https://travnik-2035.netlify.app/images/bg-slider-03.webp"></img>
+              </Modal>
+              <div onClick={() => openModal(film)} className='movie-container'>
+                <img
+                  className='movie-image'
+                  src={'https://image.tmdb.org/t/p/w500/' + (film?.poster_path || '')}
+                ></img>
+                <div>
+                  <h1 className='movie-title'>{film?.title}</h1>
+                  <p className='movie-vote'>Average vote: {film?.vote_average}</p>
+                </div>
+              </div>
             </div>
-        <div className='main'>
-            {upcomming.map((film) => {
-                return (
-                    <div key={film.id}>
-                        <Modal
-                            isOpen={modalIsOpen === film}
-                            onRequestClose={closeModal}
-                        >
-                            <div className='modalWrapper'>
-                                <img className='movie-image-modal' src={"https://image.tmdb.org/t/p/w500/" + (film?.poster_path || "")}></img>
-                                <div onClick={closeModal} class="close-container">
-                                    <div class="leftright"></div>
-                                    <div class="rightleft"></div>
-                                    <label class="close">close</label>
-                                </div>
-                                <div className='movie-text-modal'>
-                                    <h1 className='movie-title-modal'>{film?.title}</h1>
-                                    <p><b>Date of release: </b>{film?.release_date}</p>
-                                    <p className='movie-vote-modal'><b>Avrage vote: </b>{film?.vote_average}</p>
-                                    <p><b>Film overview: </b>{film?.overview}</p>
-
-                                </div>
-                                <div className='modal-video'>
-
-                                </div>
-                            </div>
-                        </Modal>
-                        <div onClick={() => openModal(film)} className="movie-container">
-                            <img className='movie-image' src={"https://image.tmdb.org/t/p/w500/" + (film?.poster_path || "")}></img>
-                            <div>
-                                <h1 className='movie-title'>{film?.title}</h1>
-                                <p className='movie-vote'>Avrage vote: {film?.vote_average}</p>
-                            </div>
-                        </div>
-                    </div>
-                )
-
-            })}
-        </div></>
-
-    )
+          );
+        })}
+      </div>
+    </>
+  );
 }
-export default Upcomming
+
+export default Upcoming;
