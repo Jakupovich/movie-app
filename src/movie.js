3e08b60aba4b53a2734dc0067132ad115e43cecc
jakupovich <nordinveca@gmail.com>
2023-04-24 17:18:55 +0200

    full

--- src/movie.js
+++ src/movie.js
@@ -2,7 +2,6 @@ import './App.css';
 import Modal from 'react-modal';
 import React, { useEffect, useState } from 'react';
 import "./modal.css"
-import Navbar from "./navbar/navbar"
 
 
 function Movies(props) {
@@ -45,7 +44,7 @@ function Movies(props) {
         onRequestClose={closeModal}
       >
         <div className='modalWrapper'>
-          <img className='movie-image-modal' src={"https://image.tmdb.org/t/p/w500/" + props.movie.poster_path}></img>
+          <img alt="" className='movie-image-modal' src={"https://image.tmdb.org/t/p/w500/" + props.movie.poster_path}></img>
           <div onClick={closeModal} class="close-container">
             <div class="leftright"></div>
             <div class="rightleft"></div>
@@ -64,7 +63,7 @@ function Movies(props) {
         </div>
       </Modal>
       <div onClick={openModal} className="movie-container">
-        <img className='movie-image' src={"https://image.tmdb.org/t/p/w500/" + props.movie.poster_path}></img>
+        <img alt="" className='movie-image' src={"https://image.tmdb.org/t/p/w500/" + props.movie.poster_path}></img>
         <div>
           <h1 className='movie-title'>{props.movie?.title}</h1>
           <p className='movie-vote'>Avrage vote: {props.movie.vote_average}</p>
